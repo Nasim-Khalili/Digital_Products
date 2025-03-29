b2cdfde38d923b601e9c6fec9aeaa0907e976186
Nasim-Khalili <khalilinassim85@gmail.com>
2025-03-29 12:13:46 +0330

    Serializer for Category and File and also Product!

--- products/serializers.py
+++ products/serializers.py
@@ -2,21 +2,29 @@ from rest_framework import serializers
 
 from .models import Category, Product, File
 
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('title','description','avatar')
-        
-        
+        fields = ('title', 'description', 'avatar')
+
+
 class FileSerializer(serializers.ModelSerializer):
+    file_type = serializers.SerializerMethodField()
+
     class Meta:
         model = File
-        fields = ('title','file')
-        
+        fields = ('id', 'title', 'file', 'file_type')
+
+    def get_file_type(self, obj):
+        return obj.get_file_type_display()
+
+
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     categories = CategorySerializer(many=True)
-    file_set = FileSerializer(many=True)
-    
+    # files = FileSerializer(many=True)
+    # foo = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ('id','title','description','avatar','categories', 'file_set','url')
+        fields = ('id', 'title', 'description', 'avatar', 'categories', 'url')
