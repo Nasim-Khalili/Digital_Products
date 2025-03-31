8cec5ff096f1043c07dd59772f52d70df6d55a1f
Nasim-Khalili <khalilinassim85@gmail.com>
2025-03-31 11:06:15 +0330

    project details. . .

--- Digital_products/settings.py
+++ Digital_products/settings.py
@@ -19,7 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    
+    'rest_framework_simplejwt',
+   
     'users',
     'products',
 ]
